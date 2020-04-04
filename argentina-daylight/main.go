cabdde84f66e2f87c9f1bdc165c19f2f9f371ed4
Pablo Lalloni <plalloni@gmail.com>
2020-04-04 17:05:28 -0300

    Improve argentina daylight

--- argentina-daylight/main.go
+++ argentina-daylight/main.go
@@ -1,54 +1,44 @@
-## Argentina daylight
-
-Yesterday one of our client-side teammates, based on the US, was very surprised about Argentina's southern location
-and its closeness to Antarctica, after looking it up on Maps out of curiosity.
-
-Quoting him, just for the sake of better representing his surprise :), he said:
-
-— "Holy crap was looking at a map"
-
-— "I did **not** realize argentina was that far south"
-
-— "What time is sunset there??"
-
-After thinking a second about Argentina's geography I answered:
-
-— "Well, we have a long latitude range, so sunrise/sunset times vary significantly"
-
-And then couldn't stop myself from wanting to tell him precisely the sunrise and sunset times of northernmost and
-southernmost locations of the country.
-
-So I ended up writing a short program which -taking advantage of a couple of external HTTP services for geo-coding and
-calculating sunrise and sunset times- prints this output:
-
-```
-La Quiaca, Municipio de La Quiaca, Departamento Yavi, Jujuy, 4650, Argentina (-22.1046124,-65.596754):
- - Summer Solstice: 06:36:22 → 20:05:40
- - Winter Solstice: 08:00:57 → 18:47:55
-Buenos Aires, Ciudad Autónoma de Buenos Aires, Argentina (-34.6075682,-58.4370894):
- - Summer Solstice: 05:38:00 → 20:06:45
- - Winter Solstice: 08:00:43 → 17:50:52
-Ushuaia, Departamento Ushuaia, Tierra del Fuego, Argentina (-54.8069332,-68.3073246):
- - Summer Solstice: 04:51:52 → 22:11:53
- - Winter Solstice: 09:58:57 → 17:11:37
-```
-
-Then -myself been trying to share Golang practicity with everyone- I thought "Hey, this short program is a nice
-little sample of idiomatic Go and good use of its awesome standard library".
-
-So I decided to share it... and you can read it below or download it from [here](argentina-daylight/main.go).
-
-To run it you just need to execute:
-
-```sh
-go run main.go
-```
-
-That's -of course- assuming that you have a Go toolkit installed.
-If you don't, just follow the instructions at https://golang.org/doc/install, I promise you'll have it installed in
-less than a minute.
-
-```go
+// Yesterday one of our client-side teammates, based on the US, was very surprised about Argentina's southern location
+// and its closeness to Antarctica, after looking it up on Maps out of curiosity.
+//
+// Quoting him, just for the sake of better representing his surprise :), he said:
+//
+// - "Holy crap was looking at a map"
+// - "I did **not** realize argentina was that far south"
+// - "What time is sunset there??"
+//
+// After thinking a second about Argentina's geography I answered:
+//
+// - "Well, we have a long latitude range, so sunrise/sunset times vary significantly"
+//
+// And then couldn't stop myself from wanting to tell him precisely the sunrise and sunset times of northernmost and
+// southernmost locations of the country.
+//
+// So I ended up writing a short program which -taking advantage of a couple of external HTTP services for geo-coding and
+// calculating sunrise and sunset times- prints this output:
+//
+//    La Quiaca, Municipio de La Quiaca, Departamento Yavi, Jujuy, 4650, Argentina (-22.1046124,-65.596754):
+//     - Summer Solstice: 06:36:22 → 20:05:40
+//     - Winter Solstice: 08:00:57 → 18:47:55
+//    Buenos Aires, Ciudad Autónoma de Buenos Aires, Argentina (-34.6075682,-58.4370894):
+//     - Summer Solstice: 05:38:00 → 20:06:45
+//     - Winter Solstice: 08:00:43 → 17:50:52
+//    Ushuaia, Departamento Ushuaia, Tierra del Fuego, Argentina (-54.8069332,-68.3073246):
+//     - Summer Solstice: 04:51:52 → 22:11:53
+//     - Winter Solstice: 09:58:57 → 17:11:37
+//
+// Then -myself been trying to share Golang practicity with everyone- I thought "Hey, this short program is a nice
+// little sample of idiomatic Go and good use of its awesome standard library".
+//
+// So I decided to share it... and here it is.
+//
+// To run it you just need to execute:
+//
+//     go run main.go
+//
+// That's -of course- assuming that you have a Go toolkit installed.
+// If you don't, just follow the instructions at https://golang.org/doc/install, I promise you'll have it installed in
+// less than a minute.
 package main
 
 import (
@@ -176,4 +166,3 @@ type Daylight struct {
 func (s *Daylight) String() string {
 	return fmt.Sprintf("%s → %s", s.Sunrise.Format("15:04:05"), s.Sunset.Format("15:04:05"))
 }
-```
